24c7bbfdd0cdff4d14614be1fcaf74e51f3c5d5f
emiliano2026 <ealvarezzanelli@gmail.com>
2026-09-07 14:46:10 -0300

    Update app.py

--- app.py
+++ app.py
@@ -305,7 +305,7 @@ with col2:
         folium.Marker(
             [float(lat), float(lon)],
             popup=f"{estacion_seleccionada}<br>Altura: {altura} msnm",
-            icon=folium.Icon(color="red", icon="cloud", width: 10px, height: 10px),
+            icon=folium.Icon(color="red", icon="cloud", width=10, height=10),
         ).add_to(m)
         st_folium(m, width=400, height=300)
     else:
