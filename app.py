e0e8c35efb02115b3f121dbb589c74639c6e5749
emiliano2026 <ealvarezzanelli@gmail.com>
2026-09-09 09:43:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import gdown
 import os
 import numpy as np
 import re
+import chardet
 
 # --- CONFIGURACIÓN ---
 st.set_page_config(page_title="Dashboard Clima IIPAC", layout="wide")
@@ -62,7 +63,7 @@ def convertir_todas_numericas(df, columnas):
 # --- 1. CARGA DE DATOS ---
 @st.cache_data
 def load_data():
-    file_id = "1J4sG2xvwR-LXCqowHhg2SMPMonvRKmzafg108eMQals" 
+    file_id = "1-WQeKO7A5_iLcS8QNIjt_iFDp2VgWEPv" 
     url = f"https://drive.google.com/uc?export=download&id={file_id}"
     output = "datos_clima_smn.csv"
     
